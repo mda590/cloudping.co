3d7113df8025705eb80534667f17472877f2b745
William Pan <will.w.pan@gmail.com>
2018-02-15 18:03:57 -0500

    Use Promises for asynchronous ping
    Wrap tcp-ping `ping()` in a Promise and execute pings in parallel using `Promise.all()`.

--- backend-monitorApp/lambda/index.js
+++ backend-monitorApp/lambda/index.js
@@ -8,7 +8,42 @@ var AWS = require('aws-sdk');
 var ddb = new AWS.DynamoDB({ region: 'us-east-2' });
 var tcpp = require('tcp-ping');
 var marshalItem = require('dynamodb-marshaler');
-var params;
+
+const regions = [
+	'ap-northeast-1',   // Tokyo
+	'ap-northeast-2',   // Seoul
+	'ap-south-1',       // Mumbai
+	'ap-southeast-1',   // Singapore
+	'ap-southeast-2',   // Sydney
+	'ca-central-1',     // Canada Central
+	'eu-central-1',     // Frankfurt
+	'eu-west-1',        // Ireland
+	'eu-west-2',        // London
+	'us-east-1',        // N Virginia
+	'us-east-2',        // Ohio
+	'us-west-1',        // N California
+	'us-west-2',        // Oregon
+	'sa-east-1',        // Brazil
+];
+
+const pingRegionAsync = function (region) {
+	return new Promise((resolve, reject) => {
+		const payload = { 
+			address: `dynamodb.${region}.amazonaws.com`,
+			port: 443,
+			attempts: 5
+		};
+
+		tcpp.ping(payload, (err, data) => {
+			if (err) reject(err);
+
+			data['timestamp'] = new Date();
+			data['regionTo'] = region;
+			console.log(`ping ${region} took an average of ${data.avg}ms.`);
+			resolve(data);
+		});
+	});
+};
 
 exports.handler = function (event, context) {
 	// region where the requests are being generated
@@ -17,137 +52,24 @@ exports.handler = function (event, context) {
 	//var region = "us-east-2";
 
 	// Use this with Lambda
-	var region = context.invokedFunctionArn.substring(15, context.invokedFunctionArn.indexOf(":506"));
+	var sourceRegion = context.invokedFunctionArn.substring(15, context.invokedFunctionArn.indexOf(":506"));
 
-	// us-east-1 (N Virginia)
-	tcpp.ping({ address: 'dynamodb.us-east-1.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-		data["region"] = region;
-		data["timestamp"] = new Date();
-		data["regionTo"] = "us-east-1";
-		params = { 'RequestItems': { 'PingTest': [{ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } }] } };
-		console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-		// us-east-2 (Ohio)
-		tcpp.ping({ address: 'dynamodb.us-east-2.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-			data["region"] = region;
-			data["timestamp"] = new Date();
-			data["regionTo"] = "us-east-2"
-			params.RequestItems.PingTest.push({ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } });
-			console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-			// us-west-1 (N California)
-			tcpp.ping({ address: 'dynamodb.us-west-1.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-				data["region"] = region;
-				data["timestamp"] = new Date();
-				data["regionTo"] = "us-west-1"
-				params.RequestItems.PingTest.push({ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } });
-				console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-				// us-west-2 (Oregon)
-				tcpp.ping({ address: 'dynamodb.us-west-2.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-					data["region"] = region;
-					data["timestamp"] = new Date();
-					data["regionTo"] = "us-west-2";
-					params.RequestItems.PingTest.push({ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } });
-					console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-					// ap-south-1 (Mumbai)
-					tcpp.ping({ address: 'dynamodb.ap-south-1.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-						data["region"] = region;
-						data["timestamp"] = new Date();
-						data["regionTo"] = "ap-south-1"
-						params.RequestItems.PingTest.push({ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } });
-						console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-						// ap-northeast-2 (Seoul)
-						tcpp.ping({ address: 'dynamodb.ap-northeast-2.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-							data["region"] = region;
-							data["timestamp"] = new Date();
-							data["regionTo"] = "ap-northeast-2"
-							params.RequestItems.PingTest.push({ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } });
-							console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-							// ap-southeast-1 (Singapore)
-							tcpp.ping({ address: 'dynamodb.ap-southeast-1.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-								data["region"] = region;
-								data["timestamp"] = new Date();
-								data["regionTo"] = "ap-southeast-1";
-								params.RequestItems.PingTest.push({ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } });
-								console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-								// ap-southeast-2 (Sydney)
-								tcpp.ping({ address: 'dynamodb.ap-southeast-2.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-									data["region"] = region;
-									data["timestamp"] = new Date();
-									data["regionTo"] = "ap-southeast-2"
-									params.RequestItems.PingTest.push({ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } });
-									console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-									// ap-northeast-1 (Tokyo)
-									tcpp.ping({ address: 'dynamodb.ap-northeast-1.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-										data["region"] = region;
-										data["timestamp"] = new Date();
-										data["regionTo"] = "ap-northeast-1"
-										params.RequestItems.PingTest.push({ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } });
-										console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-										// eu-central-1 (Frankfurt)
-										tcpp.ping({ address: 'dynamodb.eu-central-1.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-											data["region"] = region;
-											data["timestamp"] = new Date();
-											data["regionTo"] = "eu-central-1";
-											params.RequestItems.PingTest.push({ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } });
-											console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-											// eu-west-1 (Ireland)
-											tcpp.ping({ address: 'dynamodb.eu-west-1.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-												data["region"] = region;
-												data["timestamp"] = new Date();
-												data["regionTo"] = "eu-west-1"
-												params.RequestItems.PingTest.push({ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } });
-												console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-												// ca-central-1 (Canada Central)
-												tcpp.ping({ address: 'dynamodb.ca-central-1.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-													data["region"] = region;
-													data["timestamp"] = new Date();
-													data["regionTo"] = "ca-central-1"
-													params.RequestItems.PingTest.push({ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } });
-													console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-													// eu-west-2 (London)
-													tcpp.ping({ address: 'dynamodb.eu-west-2.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-														data["region"] = region;
-														data["timestamp"] = new Date();
-														data["regionTo"] = "eu-west-2"
-														params.RequestItems.PingTest.push({ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } });
-														console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-														// sa-east-1 (Brazil)
-														tcpp.ping({ address: 'dynamodb.sa-east-1.amazonaws.com', port: 443, attempts: 5 }, function (err, data) {
-															data["region"] = region;
-															data["timestamp"] = new Date();
-															data["regionTo"] = "sa-east-1"
-															params.RequestItems.PingTest.push({ 'PutRequest': { Item: marshalItem.marshalJson(JSON.stringify(data)) } });
-															console.log(region + ' to ' + data["regionTo"] + ' added to request.');
-
-															// put new test results into DynamoDB
-															ddb.batchWriteItem(params, function (err, data) {
-																if (err) console.log(err, err.stack); 				// an error occurred
-																else console.log('Data successfully written to DynamoDB. See you soon!');           	// successful response
-															});
-														});
-													});
-												});
-											});
-										});
-									});
-								});
-							});
-						});
-					});
-				});
-			});
+	Promise.all(regions.map(pingRegionAsync)).then(function (results) {
+		const params = {
+			RequestItems: {
+				PingTest: results.map((result) => {
+					result['region'] = sourceRegion;
+					return {
+						PutRequest: {
+							Item: marshalItem.marshalJson(JSON.stringify(result));
+						}
+					};
+				})
+			}
+		};
+		ddb.batchWriteItem(params, function (err, data) {
+			if (err) console.log(err, err.stack);                                     // an error occurred
+			else console.log('Data successfully written to DynamoDB. See you soon!'); // successful
 		});
 	});
 };
