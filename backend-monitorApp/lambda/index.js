e5d61f40bb100360d8f473bbaf3084cd7a36d148
pjacobs <paul@kbit.com>
2019-12-10 14:01:45 +0000

    more missing regions

--- backend-monitorApp/lambda/index.js
+++ backend-monitorApp/lambda/index.js
@@ -13,6 +13,7 @@ const regions = [
 	'ap-northeast-1',   // Tokyo
 	'ap-northeast-2',   // Seoul
 	'ap-northeast-3',   // Osaka-Local
+	'ap-east-1',        // Hong Kong
 	'ap-south-1',       // Mumbai
 	'ap-southeast-1',   // Singapore
 	'ap-southeast-2',   // Sydney
@@ -22,6 +23,8 @@ const regions = [
 	'eu-central-1',     // Frankfurt
 	'eu-west-1',        // Ireland
 	'eu-west-2',        // London
+	'eu-west-3',        // Paris
+	'eu-north-1',       // Stockholm
 	'us-east-1',        // N Virginia
 	'us-east-2',        // Ohio
 	'us-west-1',        // N California
